692847d49ae59ff622066a8aff2bc35bad1f5743
DanielJDufour <daniel.j.dufour@gmail.com>
2018-01-31 04:49:15 +0000

    added quickrun

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ number_of_chunks = 5000000000000
 CHUNK = 16 * 1024
 
 path_to_data = "/tmp/genesis"
+
+create_geojson = False
+create_table = True
+
+blacklist = ["User talk:", "Talk:", "Comments:", "User:", "File:", "Category:", "Wikinews:", "Template:", "Category talk:", "MediaWiki:", "User:"]
+non_starters = ["!", "category", "citation", "cite", "clear", "coat of arms", "collapsible", "convert", "dead", "default", "file", "flag", "flagicon", "formatnum",  "image", "incomplete", "infobox", "isbn", "lang", "main", "nomorelinks", "quote", "redirect", "see also", "template", "term-stub", "transl", "un_population", "update", "utc", "webarchive", "wikipedia", "wikt"]
--- create_data.py
+++ create_data.py
@@ -1,7 +1,7 @@
 from broth import Broth
 from bz2 import BZ2File
 from config import *
-from csv import DictReader
+from csv import DictReader, writer, QUOTE_ALL
 from datetime import datetime
 from geojson import dumps as geojson_dumps
 from geojson import Feature, Point, FeatureCollection
@@ -28,6 +28,14 @@ from wake import get_english_wikipedia_pages
 blacklist = ["User talk:", "Talk:", "Comments:", "User:", "File:", "Category:", "Wikinews:", "Template:", "Category talk:", "MediaWiki:", "User:"]
 non_starters = ["!", "category", "citation", "cite", "clear", "coat of arms", "collapsible", "convert", "dead", "default", "file", "flag", "flagicon", "formatnum",  "image", "incomplete", "infobox", "isbn", "lang", "main", "nomorelinks", "quote", "redirect", "see also", "template", "term-stub", "transl", "un_population", "update", "utc", "webarchive", "wikipedia", "wikt"]
 
+def reset_data_directory():
+    # clear old version
+    if isdir(path_to_data):
+        rmtree(path_to_data)
+
+    # create folder to store output/maps if doesn't exist
+    mkdir(path_to_data)    
+
 def load_coordinates_dictionary(save_json=False):
     
     path_to_json = "/tmp/title2coords.json"
@@ -53,16 +61,71 @@ def load_coordinates_dictionary(save_json=False):
             f.write(json.dumps(title2coords))
     
     return title2coords
+    
+def quick_run():
+    
+    try:
+
+        reset_data_directory()
+    
+        
+        url_to_gazetteer = "https://s3.amazonaws.com/firstdraftgis/wikidata-gazetteer.tsv"
+        path_to_gazetteer = download_if_necessary(url_to_gazetteer)
+        print("path_to_gazetteer:", path_to_gazetteer)    
+    
+        place_titles = set()
+        with open(path_to_gazetteer) as f:
+            for line in DictReader(f, delimiter="\t"):
+                enwiki_title = line["enwiki_title"]
+                if enwiki_title: # probably unnecessary, but playing it safe
+                    place_titles.add(enwiki_title)
+        print("created place_titles")
+
+        page_count = 0
+        
+        output_path = join(path_to_data, "genesis.tsv")
+        open(output_path, 'w').close() # create tsv file
+    
+        for page in get_english_wikipedia_pages():
+            
+            page_count += 1
+            
+            if page_count % 100000 == 0:
+                print("page_count:", page_count)
+                
+            page_id = page.find("id").text
+            #print("page_id:", page_id)
+            page_title = page.find("title").text
+            #print("page_title:", page_title)
+            page_text = page.find("revision/text").text
+            #print("page_text:", page_text)            
+
+            if page_id and page_title and page_text:
+                if page_title not in blacklist and not page_text.startswith("#REDIRECT"):
+                    #print("page_id:", page_title)
+
+                    places_in_text = set()
+
+                    # this accidentally picks up wikilinks inside of tags 
+                    for link in findall("(?<={{)[^|}]+", page_text) + findall("(?<=\[\[)[^|}\]]+", page_text):
+                        cleaned_title = clean_title(link)
+                        if cleaned_title in place_titles and ";" not in cleaned_title:
+                            places_in_text.add(cleaned_title)
+
+                    if len(places_in_text) > 1:
+                        with open(output_path, "a") as f:
+                            output_writer = writer(f, delimiter="\t", quoting=QUOTE_ALL)
+                            output_writer.writerow([page_id, ";".join(list(places_in_text))])
+                        
+        output_file.close()
+          
+    except Exception as e:
+        print(e)
 
 def run():
     try:
         
-        # clear old version
-        if isdir(path_to_data):
-            rmtree(path_to_data)
-
-        # create folder to store output/maps if doesn't exist
-        mkdir(path_to_data)
+        reset_data_directory()
 
         title2coords = load_coordinates_dictionary()
 
@@ -111,10 +174,11 @@ def run():
                                 
                         path_to_folder = join(path_to_data, page_id)
                         mkdir(path_to_folder)
-                                
-                        path_to_map = join(path_to_folder, page_id + ".geojson")
-                        with open(path_to_map, "wb") as f:
-                            f.write(map_as_string.encode("utf-8"))
+                        
+                        if create_geojson:        
+                            path_to_map = join(path_to_folder, page_id + ".geojson")
+                            with open(path_to_map, "wb") as f:
+                                f.write(map_as_string.encode("utf-8"))
     
                         path_to_text = join(path_to_folder, page_id + ".txt")
                         with open(path_to_text, "wb") as f:
@@ -123,4 +187,4 @@ def run():
     except Exception as e:
         print(e)
 
-run()
+quick_run()
